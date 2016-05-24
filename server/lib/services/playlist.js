475da08390023f0f3da99936851927ee2cc14427
Amitava saha <amitava@azuqua.com>
2016-05-24 18:49:30 +0530

    right way to create spotify client

--- server/lib/services/playlist.js
+++ server/lib/services/playlist.js
@@ -3,33 +3,36 @@
  */
 var SpotifyWebApi = require('spotify-web-api-node');
 
-var spotifyApi = new SpotifyWebApi();
 var Promise = require('bluebird');
 
 
 function getBody(resp){
-    console.log(JSON.stringify(resp.body, null, 4));
-    return Promise.resolve(resp.body)
+    return resp.body;
 }
 
-function error(err){
-    console.log(err);
-    return Promise.reject(err)
-}
 
 exports.getUserPlaylist = function(userId, accessToken){
+    
+    var spotifyApi = new SpotifyWebApi({
+        accessToken : accessToken
+    });
+
     spotifyApi.setAccessToken(accessToken);
     return spotifyApi.getUserPlaylists(userId).then(getBody)
 };
 
 exports.getFeaturedPlaylists = function (accessToken) {
-    spotifyApi.setAccessToken(accessToken);
+    var spotifyApi = new SpotifyWebApi({
+        accessToken : accessToken
+    });
 
     return spotifyApi.getFeaturedPlaylists().then(getBody);
 };
 
 exports.getPlaylistTracks = function (userId, playlistId, accessToken) {
-    spotifyApi.setAccessToken(accessToken);
+    var spotifyApi = new SpotifyWebApi({
+        accessToken : accessToken
+    });
 
     return spotifyApi.getPlaylistTracks(userId, playlistId).then(getBody);
 };
