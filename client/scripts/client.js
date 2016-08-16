b48bfc43154d4bbf633874f6a2600495451ed164
Amitava Saha <amitava82@gmail.com>
2016-08-16 07:41:48 +0530

    GA

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -11,6 +11,10 @@ import routes from './routes';
 import createStore from './redux/createStore';
 import apiClient from './helpers/api';
 
+var ReactGA = require('react-ga');
+ReactGA.initialize('UA-82539910-1');
+
+
 const createScrollHistory = useScroll(createBrowserHistory);
 const appHistory = useRouterHistory(createScrollHistory)();
 
@@ -18,13 +22,18 @@ const store = createStore(window.__INITIAL_STATE__, appHistory, new apiClient())
 
 const _routes = routes(store);
 
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
+
 class Client extends React.Component {
 
     render() {
 
         return (
             <Provider store={store}>
-                <Router history={appHistory}>
+                <Router history={appHistory} onUpdate={logPageView}>
                     {_routes}
                 </Router>
             </Provider>
